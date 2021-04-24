f879ec3be940ba9fa105d9b85fa870ad8e3141ad
shalitha <shalithaanuradha123@gmail.com>
2021-04-25 03:51:56 +0530

    Finish stage 2: UI part : Editing button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {TaskService} from './service/task.service';
+import {Priority} from './util/priority.enum';
+import {Task} from './model/task';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +10,29 @@ import {TaskService} from './service/task.service';
 })
 export class AppComponent {
 
+  editor = false;
+  disabled = true;
+  tasktext = '';
+  readonly defaultTaskTest = 'e.g., Design meeting at 11am p1 # Meeting ';
+  defaultTask = this.defaultTaskTest;
+  edit = false;
+  eventValue = -1;
+
   constructor(public taskService: TaskService) {
+  }
+
+  saveTask(task: string): void {
+    this.taskService.taskList.push(new Task(task, false, Priority.PRIORITY4));
+    this.tasktext = '';
+    this.defaultTask = this.defaultTaskTest;
+  }
+
+  editTask(event: Event, i: number): void {
+    // this.taskService.taskList.splice(i, 1);
+    // (event.target as any).value = i;
+    // console.log(i, (event.target as any).value);
+    this.eventValue = i;
+
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {TaskService} from './service/task.service';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [TaskService],
   bootstrap: [AppComponent]
